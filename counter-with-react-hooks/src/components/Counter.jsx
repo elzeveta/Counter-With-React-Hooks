ab328b4d7ddbf3791b963eab6845053a5edbb1e6
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-01 00:52:01 -0700

    Made some changes inside component

--- counter-with-react-hooks/src/components/Counter.jsx
+++ counter-with-react-hooks/src/components/Counter.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>Count {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>Count {count}</p>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    );
 };
 export default Counter;
